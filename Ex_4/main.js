cdf45ffbcb2d1db0922d494e647718873f1eebfc
Isaac Malagón <isaacmalagongarcia@gmail.com>
2025-10-22 12:11:07 +0200

    small fix

--- Ex_4/main.js
+++ Ex_4/main.js
@@ -4,6 +4,6 @@ function processarElements (array, callback) {
     }
 }
 
-const frikisEspañolesFavoritos = ["Batu", "John Cobra", "El Lion", "Michel de Motril", "Toñeca el nací"]
+const frikisEspañolesFavoritos = ["Batu", "John Cobra", "El Lion", "Michel de Motril", "Toñeca el nací", "Ramón de pitis", "El chucky de cieza"]
 
 processarElements (frikisEspañolesFavoritos, (friki) => {console.log("Puto amo: " +  friki)});
