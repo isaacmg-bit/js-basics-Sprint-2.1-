91c1df41c9f7ef2e88dad1366d93ad9d06781896
Isaac Malagón <isaacmalagongarcia@gmail.com>
2025-10-25 21:24:10 +0200

    folder fix

--- 1.7/Ex_6/main.js
+++ 1.7/Ex_6/main.js
@@ -1,9 +1,9 @@
 const promiseOne = new Promise((resolve) => {
-  setTimeout(() => resolve('First promise ready (2s)'), 2000);
+  setTimeout(() => resolve('First promise ready (2sec)'), 2000);
 });
 
 const promiseTwo = new Promise((resolve) => {
-  setTimeout(() => resolve('Second promise ready (3s)'), 3000);
+  setTimeout(() => resolve('Second promise ready (3sec)'), 3000);
 });
 
 Promise.all([promiseOne, promiseTwo])
